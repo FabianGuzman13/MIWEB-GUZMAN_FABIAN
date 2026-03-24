ef6d0eb6007c15ed340d8afbe297eec4f52454e9
Fabian Enrique Guzman Choque <fabian@example.com>
2026-03-23 21:48:56 -0500

    WEB1: Solo fibonacci

--- index.php
+++ index.php
@@ -14,8 +14,6 @@
     
     <h2>Opciones</h2>
     <ul>
-        <li><a href="suma.php">Calcular Suma de 2 Números</a></li>
-        <li><a href="factorial.php">Calcular Factorial de un Número</a></li>
         <li><a href="fibonacci.php">Serie Fibonacci</a></li>
     </ul>
 </body>
